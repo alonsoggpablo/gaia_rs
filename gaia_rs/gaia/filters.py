a4e2748af0affe190cf1fe4ef15e5d4621fd2f97
Pablo Alonso García <alonsogpablo@uniovi.es>
2024-09-05 10:29:35 +0200

    Ajustes en requirements y filters para evitar fallos antes de migracion inicial

--- gaia_rs/gaia/filters.py
+++ gaia_rs/gaia/filters.py
@@ -2,23 +2,31 @@
 
 from django_filters import FilterSet, ModelChoiceFilter
 from .models import DataCube, DataProduct, Category
+from django.db.utils import OperationalError
 
 
 class DataCubeFilter(FilterSet):
     class Meta:
         model = DataCube
-        fields = {'dataproduct':['exact']}
+        fields = {'dataproduct': ['exact']}
 
 
 class DataProductFilter(FilterSet):
-    class Meta:
-        model = DataProduct
-        fields = {'category':['exact']}
     def get_categories():
-        dataproducts=DataProduct.objects.all()
-        categories=[]
-        for dataproduct in dataproducts:
-            categories.append(dataproduct.category)
+        dataproducts = []
+        try:
+            dataproducts = DataProduct.objects.all()
+        except: return[]
+        categories = []
+        try:
+            for dataproduct in dataproducts:
+                categories.append(dataproduct.category)
+        except:pass
         return categories
 
-    category=ModelChoiceFilter(queryset=Category.objects.filter(category__in=get_categories()).order_by('category').distinct())
+    category = ModelChoiceFilter(queryset=Category.objects.filter(
+        category__in=get_categories()).order_by('category').distinct())
+
+    class Meta:
+        model = DataProduct
+        fields = {'category': ['exact']}
